7102de9d0cb4154a4937fb457db2a61e492d7d05
Foctser <mr.gipl@mail.ru>
2025-03-28 10:12:01 +0300

    Измененный вариант

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
-import "./App.css";
+import "./Home.css";
 
 function HomePage() {
   const [games, setGames] = useState([]);
